259e85a8afdbf392d4b72d472303d510a6b570ee
Youssef & Jeff <student@epicodus.com>
2015-10-19 19:16:15 -0700

    Make spec for year 400 pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var leapYear = function(year) {
-  if (year % 100 === 0) {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
     return false;
   } else if (year % 4 === 0) {
     return true;
