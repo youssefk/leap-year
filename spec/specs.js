24f2bcbaf176eb8db4864c22f10c5e64f2b8147e
Youssef & Jeff <student@epicodus.com>
2015-10-19 18:58:38 -0700

    See spec fail second statement

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('leapYear', function() {
   it("is false for a year that is not divisible by 4", function() {
     expect(leapYear(1999)).to.equal(false);
   });
+
+  it ("is true for most years divisible by 4", function() {
+    expect(leapYear(2012)).to.equal(true);
+  });
 });
