73b9a8f8130fafb093c3648d66b9d658154f9483
Youssef & Jeff <student@epicodus.com>
2015-10-19 19:06:15 -0700

    make the third spec pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var leapYear = function(year) {
-  if (year % 4 === 0) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
     return true;
   } else {
-  return false;
+    return false;
   }
 };
