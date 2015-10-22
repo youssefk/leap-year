8575fafae3f547672aa1e5c424874cb0574f9cad
Youssef <youssefkhalifa@live.com>
2015-10-21 18:13:40 -0700

    Refactor code even further.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 var leapYear = function(year) {
-  if ((year % 400 === 0) && (year % 100 === 0)) {
-    return true;
-  } else if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if (((year % 400 === 0) && (year % 100 === 0)) || ((year % 4 === 0) && (year % 100 !== 0))) {
     return true;
   } else {
     return false;
